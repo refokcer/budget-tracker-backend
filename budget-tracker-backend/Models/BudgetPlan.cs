a8ee34031469ae26909844abd63f8251dce64802
refokcer <refokcer@gmail.com>
2025-01-26 18:34:09 +0200

    Edited Budget plan

--- budget-tracker-backend/Models/BudgetPlan.cs
+++ budget-tracker-backend/Models/BudgetPlan.cs
@@ -5,9 +5,11 @@
         public int Id { get; set; }
         public int CategoryId { get; set; }
         public decimal Amount { get; set; }
+        public int CurrencyId { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
-        public string Comment { get; set; }
+        public string Type { get; set; } = null!; // enum of monthly or event
+        public string? Description { get; set; }
 
         // Навигационные свойства
         public virtual Category Category { get; set; } = new Category();
