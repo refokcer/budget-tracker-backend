78fc16fe85c8c42bb294e6ab3c1b2e654caaf179
refokcer <refokcer@gmail.com>
2025-01-29 23:28:46 +0200

    Edited Category now get Type

--- budget-tracker-backend/Models/Category.cs
+++ budget-tracker-backend/Models/Category.cs
@@ -4,6 +4,7 @@
     {
         public int Id { get; set; }
         public string Title { get; set; } = null!;
+        public string Type { get; set; } = null!; // Enum of => Income, Expense
         public string? Description { get; set; }
     }
 }
