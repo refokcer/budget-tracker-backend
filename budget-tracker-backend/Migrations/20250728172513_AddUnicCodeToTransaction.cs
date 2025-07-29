c280c7b2d1b5d71a4b9cdd037f54dbb46f706b5f
refokcer <refokcer@gmail.com>
2025-07-29 10:52:48 +0300

    add migr

---
+++ budget-tracker-backend/Migrations/20250728172513_AddUnicCodeToTransaction.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace budget_tracker_backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUnicCodeToTransaction : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "UnicCode",
+                table: "Transactions",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UnicCode",
+                table: "Transactions");
+        }
+    }
+}
